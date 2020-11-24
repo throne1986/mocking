5e4637c8d45df6497f7c34b861a93b74be983c10
Bonnie Schulkin <biz@bonnieschulkin.com>
2020-11-23 20:34:55 -0800

    Updated tests to use moxios

--- src/__test__/Auth/login.test.js
+++ src/__test__/Auth/login.test.js
@@ -4,34 +4,35 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from 'react-router-dom';
 import Login from '../../pages/Auth/Login';
 import '@testing-library/jest-dom/extend-expect';
+import moxios from 'moxios';
+import instance from '../../utils/api';
 
 import { mockLogin } from '../../utils/api';
-import axios from 'axios'
-
   
+  // jest.mock('axios', () => {
+  //   return {
+  //     interceptors: {
+  //       request: { use: jest.fn(), eject: jest.fn() },
+  //       response: { use: jest.fn(), eject: jest.fn() },
+  //     },
+  //   };
+  // });
   
-  jest.mock('axios', () => {
-    return {
-      interceptors: {
-        request: { use: jest.fn(), eject: jest.fn() },
-        response: { use: jest.fn(), eject: jest.fn() },
-      },
-    };
-  });
-  
-  jest.mock('axios', () => ({
-    __esModule: true,
-    create: jest.fn(),
-    get: jest.fn(),
-    post: jest.fn(),
-    default:jest.fn()
-  }));
+  // jest.mock('axios', () => ({
+  //   __esModule: true,
+  //   create: jest.fn(),
+  //   get: jest.fn(),
+  //   post: jest.fn(),
+  //   default: { create: jest.fn() }
+  // }));
   
   jest.mock("../../utils/api");
 
 
 describe("Login form submit information with validated data", () => {
   beforeEach(() => {
+    moxios.install(instance);
+
     render(
     <Router>
         <Login  />
@@ -39,7 +40,9 @@ describe("Login form submit information with validated data", () => {
         
         );
   });
-  
+  afterEach(() => {
+    moxios.uninstall(instance)
+  })  
 
   it("should not display error when value is valid", async () => {
     fireEvent.input(
